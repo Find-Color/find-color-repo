f845bb29c84d8eb10b8d811e06c7b548e98c6456
Mathew <matsantos114@gmail.com>
2023-06-13 17:46:27 -0400

    all tables done

--- src/db/migrations/20230613212436_create_upvotes_table.js
+++ src/db/migrations/20230613212436_create_upvotes_table.js
@@ -3,12 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = (knex) => {
-  table.integer("post_id").references("post_id").inTable("posts");
-  table.integer("user_id").references("user_id").inTable("users");
+  return knex.schema.createTable("upvotes", (table) => {
+    table.integer("post_id").references("post_id").inTable("posts");
+    table.integer("user_id").references("user_id").inTable("users");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = (knex) => {};
+exports.down = knex => knex.schema.dropTable("upvotes");
--- src/db/migrations/20230613213838_create_comments_table.js
+++ src/db/migrations/20230613213838_create_comments_table.js
@@ -3,17 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = (knex) => {
+  return knex.schema.createTable("comments", (table) => {
     table.increments("comment_id");
     table.integer("post_id").references("post_id").inTable("posts");
     table.integer("user_id").references("user_id").inTable("users");
     table.string("comment_text");
     table.timestamp("created_at");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = (knex) => {
-
-};
+exports.down = knex => knex.schema.dropTable("comments");
