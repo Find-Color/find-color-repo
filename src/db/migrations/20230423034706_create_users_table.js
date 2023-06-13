fbd25f9c3c2b0c81ee2dc29c3f099bebcd24cd07
duracellB <i.schlae.h@gmail.com>
2023-06-13 18:09:12 -0400

    edited table commit

--- src/db/migrations/20230423034706_create_users_table.js
+++ src/db/migrations/20230423034706_create_users_table.js
@@ -2,16 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = knex =>
-   knex.schema.createTable("users", table => {
+exports.up = knex => {
+  return knex.schema.createTable("users", table => {
     table.increments("user_id");
     table.string("username").notNullable().unique();
     table.string("password_hash").notNullable();
-    table.string("image").notNullable().unique();
+    table.string("image");
     table.timestamps(true, true);
     table.string("location");
     table.integer("coverage_score");
   });
+};
 
 /**
  * @param { import("knex").Knex } knex
