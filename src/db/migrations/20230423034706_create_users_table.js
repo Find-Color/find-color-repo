eab1b490ce20d59614960531b1f70f0be05553e7
Mathew <matsantos114@gmail.com>
2023-06-13 16:04:13 -0400

    added image to users column

--- src/db/migrations/20230423034706_create_users_table.js
+++ src/db/migrations/20230423034706_create_users_table.js
@@ -7,6 +7,7 @@ exports.up = knex =>
     table.increments("user_id");
     table.string("username").notNullable().unique();
     table.string("password_hash").notNullable();
+    table.string("image").notNullable().unique();
     table.timestamps(true, true);
     table.string("location");
     table.integer("coverage_score");
