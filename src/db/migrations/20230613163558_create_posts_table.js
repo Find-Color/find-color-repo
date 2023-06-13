88318a45a636f0c4e7c66810248bc1fbd73d5c44
Mathew <matsantos114@gmail.com>
2023-06-13 15:53:55 -0400

    added personal_information table into posts

--- src/db/migrations/20230613163558_create_posts_table.js
+++ src/db/migrations/20230613163558_create_posts_table.js
@@ -19,6 +19,8 @@ exports.up = knex => {
     table.string("gender");
     table.integer("age");
     table.string("image");
+    table.string("description_text");
+    table.string("contact_info");
     table.integer("user_id").references("user_id").inTable("users");
   });
 };
