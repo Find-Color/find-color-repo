ef851e0390e0dbf023d3d1aee7bb5e3f46cfde8a
duracellB <i.schlae.h@gmail.com>
2023-06-14 19:34:54 -0400

    connecting feed front and back end

--- frontend/src/components/MissingPeopleList.jsx
+++ frontend/src/components/MissingPeopleList.jsx
@@ -1,17 +1,21 @@
-import MissingPersonCard from "./missingPersonCard"
-import dummyData from "../dummyData"
-console.log(dummyData)
+import MissingPersonCard from "./missingPersonCard";
+import { getAllPosts } from "../adapters/post-adapter";
+import { useState, useEffect } from "react";
 
-export default function MissingPeopleList(){
+export default function MissingPeopleList() {
+  const [posts, setPosts] = useState([]);
 
-    return(
-        <>
-        <div className="ui centered cards missing-people-container">
-            {dummyData.map((person, i)=>{
-                return <MissingPersonCard key={i} {...person}/>
-            })}
-        </div>
-        
-        </>
-    )
+  useEffect(() => {
+    getAllPosts().then(setPosts);
+  }, []);
+
+  return (
+    <>
+      <div className="ui centered cards missing-people-container">
+        {posts.map((person, i) => {
+          return <MissingPersonCard key={i} {...person} />;
+        })}
+      </div>
+    </>
+  );
 }
--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,7 +1,9 @@
 import MissingPersonCard from "../components/missingPersonCard";
 import MissingPeopleList from "../components/MissingPeopleList";
+import { useEffect } from "react";
 
 export default function Feed() {
+
   return (
     <>
       <MissingPeopleList />
