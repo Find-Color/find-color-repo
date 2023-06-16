85f6a45ea8baa9c36ba5bcfdfc84694840dac681
ivoherre <i.herrera98@hotmail.com>
2023-06-16 12:50:12 -0400

    hi

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+
+const Footer = () => {
+    return (
+       <div id= "footer">
+        <div id= "footercol1">
+            <div>
+                <h1>Find Color</h1>
+            </div>
+            <div>
+                <p>Latest Blog Post</p>
+                <h2>Ready to get started?</h2>
+                <p>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+        <div>
+            <div id= "footercol2">
+                <p>Product</p>
+                <p>Product</p>
+                <p>Product</p>
+                <p>Product</p>
+                <p>Product</p>
+                <p>Company</p>
+                <p>Company</p>
+                <p>Company</p>
+                <p>Company</p>
+                <p>Company</p>
+            </div>
+            <div>
+                <p>© 2010 — 2020   Privacy — Terms</p>
+            </div>
+        </div>
+       </div> 
+    )
+}
+
+export default Footer
