d7274b190c5d02bae9bb9a46091a3bae583c677d
duracellB <i.schlae.h@gmail.com>
2023-06-17 15:19:45 -0400

    started controllers

---
+++ src/controllers/comment/create.js
@@ -0,0 +1,11 @@
+const create = async (req, res) => {
+  const {
+    session,
+    db: { Comment },
+    body: { post_id, user_id, comment_text },
+  } = req;
+  const comment = await Comment.create(post_id, user_id, comment_text);
+  res.send(comment);
+};
+
+module.exports = create;
