5faa7eedabea7602ffc2bcfc0caae6b16d6c42e5
duracellB <i.schlae.h@gmail.com>
2023-06-19 09:55:36 -0400

    commenting out just to get rid of working errors

--- src/controllers/comment/index.js
+++ src/controllers/comment/index.js
@@ -1,19 +1,19 @@
 const create = require("./create");
-const find = require("./find");
-const list = require("./listAll");
-const listFromPost = require("./listFromPost");
-const listFromUser = require("./listFromUser");
-const update = require("./update");
-const deleteAllFromPost = require("./deleteAllFromPost");
-const deleteAllFromUser = require("./deleteAllFromUser");
+// const find = require("./find");
+// const list = require("./listAll");
+// const listFromPost = require("./listFromPost");
+// const listFromUser = require("./listFromUser");
+// const update = require("./update");
+// const deleteAllFromPost = require("./deleteAllFromPost");
+// const deleteAllFromUser = require("./deleteAllFromUser");
 
 module.exports = {
   create,
-  find,
-  list,
-  listFromPost,
-  listFromUser,
-  update,
-  deleteAllFromPost,
-  deleteAllFromUser,
+  // find,
+  // list,
+  // listFromPost,
+  // listFromUser,
+  // update,
+  // deleteAllFromPost,
+  // deleteAllFromUser,
 };
