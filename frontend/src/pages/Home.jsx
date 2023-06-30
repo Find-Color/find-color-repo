ecf61c5c8de4e7588685652309d50940934bdabd
Mathew <matsantos114@gmail.com>
2023-06-29 22:10:17 -0400

    final touch

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -30,46 +30,21 @@ export default function HomePage() {
         />
         {/* <a href="https://storyset.com/job">Job illustrations by Storyset</a> */}
       </section>
-      {/* <section id="bnwimageparent">
-        <Typography className="bnwText" variant="h1">
-          "Darkness cannot drive out darkness; only light can do that. Hate
-          cannot drive out hate; only love can do that." –Martin Luther King Jr.
-        </Typography>
-        <img className="collage" src="./src/images/collage.png" alt="" />
-      </section> */}
-
-      {/* Indias blank template */}
-
-      {/* <section className="className4">
-        <img
-          src="./src/images/landing-page-3.jpg"
-          alt=""
-          className="landing-page-5"
-        />
-      </section> */}
 
       <section className="coverageWorth ">
-        <div className="coverageWorthText">
-          <Typography variant="h1">
-            How much coverage are YOU worth?
-          </Typography>
-          {/* <Typography variant="lead">
-            Your news coverage depends on your demographics yada yada we gotta
-            do a lil blurb here that segways into take the quiz
-          </Typography> */}
-        </div>
-
         <div className="coverageWorthButton">
+          <Typography variant="h1">How much coverage are YOU worth?</Typography>
           <a target="_blank" href="https://areyoupressworthy.com/">
             <Button color="red">Take the quiz now</Button>
           </a>
         </div>
-
-        <img
-          src="./src/images/landing-page-3.jpg"
-          alt=""
-          className="landing-page-3"
-        />
+        <div className="coverageWorthImage">
+          <img
+            src="./src/images/coverage.png"
+            alt="Breaking News"
+            className="landing-page-3"
+          />
+        </div>
       </section>
     </>
   );
